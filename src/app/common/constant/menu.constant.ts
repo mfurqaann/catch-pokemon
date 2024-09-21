dbe65c3b5977bfeb2d4cf02cc70dca70dd0cc444
mfurqaann <furqan1061@gmail.com>
2024-09-21 19:01:55 +0700

    refactor(pokemon): add switch language constant

--- src/app/common/constant/menu.constant.ts
+++ src/app/common/constant/menu.constant.ts
@@ -2,4 +2,5 @@ export class MenuConstant {
   static readonly ALL_POKEMON = 'MENU.ALL_POKEMON';
   static readonly MY_POKEMON = 'MENU.MY_POKEMON';
   static readonly RANDOM_POKEMON = 'MENU.RANDOM_POKEMON';
+  static readonly SWITCH_LANGUAGE = 'MENU.SWITCH_LANGUAGE';
 }
